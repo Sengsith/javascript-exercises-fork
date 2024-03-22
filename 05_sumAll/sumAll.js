454dcebee5d55bd58d81d913cec19ad98f73b14c
Angelo Rosario <angelorosario10@gmail.com>
2024-03-21 17:33:26 -0700

    complete 05_sumAll
    brute force

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0 || typeof x !== "number" || typeof y !== "number") {
+    return "ERROR";
+  }
 
+  let result = 0;
+
+  if (x < y) {
+    for (let i = x; i <= y; i++) {
+      result += i;
+    }
+  } else {
+    for (let i = y; i <= x; i++) {
+      result += i;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
