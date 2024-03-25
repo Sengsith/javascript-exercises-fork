2242e21d6380276155b9b5bc129a50c52dc72050
Angelo Rosario <angelorosario10@gmail.com>
2024-03-25 16:50:40 -0700

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+  if (x === 0) return 1;
+  return x * factorial(x - 1);
 };
+// 3 = 3 * 2 * 1
+// Start at x
+// Loop until we hit 1
+// Sum = x * x-1
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
