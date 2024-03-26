ba7fa91c73a4dd2d3e8de934b5646a79b6e4ca62
Angelo Rosario <angelorosario10@gmail.com>
2024-03-25 17:20:00 -0700

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let punctuation = /[/.,?! ]/g;
+  let stripped = str.replace(punctuation, "");
+  let reverse = stripped.split("").reverse().join("");
+  console.log(`stripped: ${stripped}`);
+  console.log(`reverse: ${reverse}`);
+  return reverse.toLowerCase() == stripped.toLowerCase();
 };
 
 // Do not edit below this line
